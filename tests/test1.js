a8ee6d21baf54466a7572489cb46c55490cb635e
Marketionist <marketionist@gmail.com>
2020-05-27 00:00:56 -0400

    Update linting and tests

--- tests/test1.js
+++ tests/test1.js
@@ -1,36 +1,38 @@
+const path = require('path');
 const { stamp, createRequest, readDirectories } = require('../index.js');
 
+// Test randomDigits
 const randomDigits = stamp.getTimestamp();
+
 console.log(
     'process.env.TIMESTAMP: ' +
     `${randomDigits === process.env.TIMESTAMP} ${process.env.TIMESTAMP}`
 );
 
 const newRandomDigits = stamp.resetTimestamp();
+
 console.log(
     'process.env.TIMESTAMP: ' +
     `${newRandomDigits === process.env.TIMESTAMP} ${process.env.TIMESTAMP}`
 );
 
-const responseGet = await createRequest(
-    'GET',
-    'https://www.google.com/',
-    '',
-    ''
-);
-console.log(`responseGet: ${responseGet}`);
+(async () => {
+    // Test createRequest
+    const responsePost = await createRequest(
+        'POST',
+        'http://httpbin.org/post',
+        '{ "Content-Type": "application/json", "Authorization": "Bearer aBcD1234" }',
+        '{ "test1": 1, "test2": 2 }'
+    );
 
-const responsePost = await createRequest(
-    'POST',
-    'http://httpbin.org/post',
-    '{ "Content-Type": "application/json", "Authorization": "Bearer aBcD1234" }',
-    '{ "test1": 1, "test2": 2 }'
-);
-console.log(`responsePost: ${responsePost}`);
+    console.log(`responsePost: ${responsePost}`);
 
-const pathToDirectory1 = path.join(__dirname, '../');
-const pathToDirectory2 = path.join(__dirname);
+    // Test readDirectories
+    const pathToDirectory1 = path.join(__dirname, '../utils');
+    const pathToDirectory2 = path.join(__dirname);
 
-const allFiles = await readDirectories([pathToDirectory1, pathToDirectory2]);
-console.log(`allFiles: ${allFiles}`);
+    const allFiles = await readDirectories([pathToDirectory1, pathToDirectory2]);
+
+    console.log(`allFiles: ${allFiles}`);
+})();
 
