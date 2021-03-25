365d9cd51af30740e8a30d636f9fa88b8ac164c9
Marketionist <marketionist@gmail.com>
2021-03-24 21:26:09 -0400

    Update tests

--- tests/test1.js
+++ tests/test1.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { stamp, createRequest, readDirectories } = require('../index.js');
+const { stamp, createRequest, sendRequest, readDirectories } = require('../index.js');
 
 // Test randomDigits
 const randomDigits = stamp.getTimestamp();
@@ -29,7 +29,18 @@ console.log(
         logLevel
     );
 
-    console.log(`responsePost: ${responsePost}`);
+    console.log(`responsePost from createRequest: ${responsePost}`);
+
+    // Test sendRequest
+    const responsePost2 = await sendRequest({
+        requestUrl: 'http://a:b@httpbin.org/post?query=string',
+        method: 'POST',
+        headersString: '{ "Content-Type": "application/json", "Authorization": "Bearer EfGh5678" }',
+        logLevel: logLevel,
+        bodyString: '{ "test3": 3, "test4": 4 }'
+    });
+
+    console.log(`responsePost from sendRequest: ${responsePost2}`);
 
     // Test readDirectories
     const pathToDirectory1 = path.join(__dirname, '..', 'utils');
