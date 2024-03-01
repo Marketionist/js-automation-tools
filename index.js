90d6cc7bdc4e9a684707681679b13edda5c9792e
Marketionist <marketionist@gmail.com>
2024-03-01 07:26:13 -0500

    Add generateDateTime, generateDateTimePlusHours

--- index.js
+++ index.js
@@ -4,11 +4,13 @@
 
 const readDirectories = require('./utils/read-directories.js');
 const stamp = require('./utils/stamp.js');
+const dateTime = require('./utils/date-time.js');
 const sendRequest = require('./utils/send-request.js');
 
 module.exports = {
     readDirectories: readDirectories,
     stamp: stamp,
+    dateTime: dateTime,
     sendRequest: sendRequest,
     createRequest: sendRequest
 };
