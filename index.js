597800670807cdcc9a46af048ca7c5d09e8c9ac7
Marketionist <marketionist@gmail.com>
2024-07-17 08:03:14 -0400

    Export retryIfFalse

--- index.js
+++ index.js
@@ -5,14 +5,14 @@
 const readDirectories = require('./utils/read-directories.js');
 const stamp = require('./utils/stamp.js');
 const dateTime = require('./utils/date-time.js');
-const asyncRetryIfFalse = require('./utils/async-retry.js');
+const retryIfFalse = require('./utils/retry.js');
 const sendRequest = require('./utils/send-request.js');
 
 module.exports = {
     readDirectories: readDirectories,
     stamp: stamp,
     dateTime: dateTime,
-    asyncRetryIfFalse: asyncRetryIfFalse,
+    retryIfFalse: retryIfFalse,
     sendRequest: sendRequest,
     createRequest: sendRequest
 };
